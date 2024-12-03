c4cb890cc1caf2d456b1f098902dd9e41db01b88
gmoraes16 <gsamoraes16@gmail.com>
2024-12-03 18:09:27 -0300

    Update Implementação de MergeSort.c

--- Implementação de MergeSort.c
+++ Implementação de MergeSort.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void Intercala(int p, int q, int r, int v[]) {
     int i, j, k, *w;
-    w = malloc((r-p) * sizeof(int));
+    w = malloc((r - p + 1) * sizeof(int));
     i = p; j = q; k = 0;
     while (i < q && j < r) {
         if (v[i] <= v[j]) w[k++] = v[i++];
@@ -11,7 +12,7 @@ void Intercala(int p, int q, int r, int v[]) {
     }
     while (i < q) w[k++] = v[i++];
     while (j < r) w[k++] = v[j++];
-    for (i = p; i < r; i++) v[i] = w[i-p];
+    for (i = p; i < r; i++) v[i] = w[i - p];
     free(w);
 }
 
@@ -25,18 +26,23 @@ void Mergesort(int p, int r, int v[]) {
 }
 
 int main() {
-    int n = 50000;  // Número de elementos
+    int n = 50000;  // Numero de elementos
     int v[n];
 
-    // Gerador de números aleatórios
+    // Gera numeros aleatorios
     srand(time(NULL));
 
-    Mergesort(0, t, v);
+ for (int i = 0; i < n; i++) {
+        v[i] = rand();
+    }
     
-    for (int i = 0; i < t; i++) {
+    Mergesort(0, n, v);
+    
+    for (int i = 0; i < n; i++) {
         printf("%d ", v[i]);
     }
     printf("\n");
+
     return 0;
 }
 
