05cb39e15153d3ba68e2416b2d6767f5e132664f
gmoraes16 <gsamoraes16@gmail.com>
2025-01-30 19:33:34 -0300

    Add files via upload

---
+++ 1930 - Tomadas.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int T1, T2, T3, T4, resultado;
+    
+    scanf("%d %d %d %d", &T1, &T2, &T3, &T4);
+    
+    resultado = T1 + T2 + T3 + T4 -3;
+    
+    printf("%d\n", resultado);
+ 
+    return 0;
+}
